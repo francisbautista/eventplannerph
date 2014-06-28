48c0e62b3624987ae1cad189b46b8699750facdd
Francis Bautista <francis.bautista07@gmail.com>
2014-06-28 16:43:13 +0800

    Fixed routes for conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,26 @@
 Rails.application.routes.draw do
 
+  %w[index about contact].each do |page|
+      get page, controller: 'pages', action: page
+  end
 
-	devise_for :suppliers
-	devise_for :users
+  get "/dashboard", to: "pages#dashboard", as: :dashboard
+  root to: "pages#index"
 
-	resources :amenities
+  devise_for :suppliers
+  devise_for :users
 
+  resources :pages
+  resources :event_types
 
+  resources :amenities
 	resources :venues do
 		resources :rooms do
 			resources :bookings
 		end
 	end
 
-	resources :pages
-	resources :event_types
 
-
-	%w[index about contact].each do |page|
-		get page, controller: 'pages', action: page
-	end
-
-	get 'pages/index'
-	get "/dashboard", to: "pages#dashboard", as: :dashboard
-	root to: "pages#index"
 
 
 end
-
