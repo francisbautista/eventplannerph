bfa0940551ccf722cf628f1f4fc6fb57c6c68ed9
Francis Bautista <francis.bautista07@gmail.com>
2014-06-28 15:23:02 +0800

    Created EventType model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :event_types
+
   resources :bookings
 
   resources :rooms
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140628065333) do
+ActiveRecord::Schema.define(version: 20140628072240) do
 
   create_table "bookings", force: true do |t|
     t.string   "title"
@@ -27,6 +27,12 @@ ActiveRecord::Schema.define(version: 20140628065333) do
     t.integer  "venue_id"
     t.integer  "room_id"
     t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "event_types", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
