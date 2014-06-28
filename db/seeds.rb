7c0b58bc35c75d7615b7ac5a054d26fa8479887c
Francis Bautista <francis.bautista07@gmail.com>
2014-06-28 16:20:52 +0800

    modified seeds and schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create!(first_name: 'Francis', last_name: 'Bautista',
+            email: 'francis.bautista07@gmail.com', password: 'password')
+User.create!(first_name: 'Yuri', last_name: 'Kristoff',
+            email: 'yuri@gmail.com', password: 'password')
+User.create!(first_name: 'Angelo', last_name: 'Francisco',
+            email: 'afranciscowork@gmail.com', password: 'password')
+User.create!(first_name: 'Tori', last_name: 'Tadiar',
+            email: 'tadiar.victoria@gmail.com', password: 'password')
+User.create!(first_name: 'Eyana', last_name: 'Mallari',
+            email: 'eyanamallari@gmail.com', password: 'password')
+
+Supplier.create!(first_name: 'Francis', last_name: 'Bautista',
+            email: 'francis.bautista07@gmail.com', password: 'password')
+Supplier.create!(first_name: 'Yuri', last_name: 'Kristoff',
+            email: 'yuri@gmail.com', password: 'password')
+Supplier.create!(first_name: 'Angelo', last_name: 'Francisco',
+            email: 'afranciscowork@gmail.com', password: 'password')
+Supplier.create!(first_name: 'Tori', last_name: 'Tadiar',
+            email: 'tadiar.victoria@gmail.com', password: 'password')
+Supplier.create!(first_name: 'Eyana', last_name: 'Mallari',
+            email: 'eyanamallari@gmail.com', password: 'password')
