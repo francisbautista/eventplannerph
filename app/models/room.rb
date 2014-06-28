d93fae34fd2e3bc6f40df7bee1e03f08eaa5a05f
Angelo Francisco <afranciscowork@gmail.com>
2014-06-28 15:07:05 +0800

    Created validations for rooms

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,11 @@
 class Room < ActiveRecord::Base
+    
+CLASSIFICATION = ["Room","Ballroom","Hall",
+        "Garden","Auditorium", "Ampitheater",
+        "Gallery", "Covered Courts","Field"]
+
+    validates :name, presence: true
+    validates :classification, presence: true, inclusion: { in: CLASSIFICATION}
+    validates :capacity, numericality: { only_integer: true }
+
 end
