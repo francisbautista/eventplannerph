3069589776bb57a8b383fb3e8415366ff595e42d
Eyana <eyana.mallari@obf.ateneo.edu>
2014-06-28 16:03:12 +0800

    added pages views (dashboard and landing) and controller. routes modified

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-        
+       
 	resources :amenities
-	
+	resources :pages
 	resources :venues do
 		resources :rooms do
 		    resources :bookings
 		end
 	end
-
-
 	resources :event_types
+	root to: "pages#index"
 end
