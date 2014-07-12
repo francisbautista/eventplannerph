e487135b9cb62d9d53823a5e03e72686afc6300e
Angelo Francisco <afranciscowork@gmail.com>
2014-07-12 17:36:04 +0800

    Added assets to resource routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,32 @@
 Rails.application.routes.draw do
 
- 	# get "/dashboard", to: "pages#dashboard", as: :dashboard
+  # get "/dashboard", to: "pages#dashboard", as: :dashboard
 
   #--Page Controller Resource Nesting--------#
   %w[index about contact dashboard].each do |page|
-      get page, controller: 'pages', action: page
+    get page, controller: 'pages', action: page
   end
 
   #--Page Root Declaration-------------------#
   root to: "pages#index"
 
   #--Login Resource Nesting via Devise-------#
-	devise_for :suppliers
-	devise_for :users
+  devise_for :suppliers
+  devise_for :users
 
   #--Navigation Resource Declarations--------#
-	resources :amenities
+  resources :amenities
   resources :assets
   resources :pages
   resources :event_types
 
   #--Navigation Resource Nesting-------------#
-	resources :venues do
+  resources :venues do
     resources :assets
-		resources :rooms do
-			resources :bookings
-		end
-	end
+    resources :rooms do
+      resources :assets
+      resources :bookings
+    end
+  end
 
 end
